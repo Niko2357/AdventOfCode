054375394fcd5dbcb2661300b787fce51baa525c
Niko2357 <133783612+Niko2357@users.noreply.github.com>
2023-12-31 17:51:40 +0100

    Add files via upload

---
+++ Day4/src/Cards.java
@@ -0,0 +1,125 @@
+import java.util.ArrayList;
+
+public class Cards {
+
+    ArrayList<String> winning;
+    ArrayList<String> nums;
+    ArrayList<Integer> sumOfPoints;
+
+
+    public Cards() {
+        this.winning = new ArrayList<>();
+        this.nums = new ArrayList<>();
+        sumOfPoints = new ArrayList<>();
+    }
+
+    /**
+     * Splits card by non digit characters and adds
+     * winning numbers into list of winning numbers
+     * and other numbers adds into list of nums.
+     * Numbers are String.
+     *
+     * @param a Line of numbers(card).
+     */
+    public void split(String a) {
+        winning = new ArrayList<>();
+        nums = new ArrayList<>();
+        String[] parts = a.split("\\D+");
+        for (int j = 0; j < 11; j++) {
+                winning.add(parts[j]);
+            }
+        for (int p = 11; p < 35; p++) {
+                nums.add(parts[p]);
+            }
+    }
+    int matches = 0;
+
+    /**
+     * Searches for same number in 'nums' list as in the 'winning' list.
+     *
+     * @return int Quantity of matched numbers.
+     */
+    public int searchWinNum(){
+        matches = 0;
+        for(String i: winning){
+            if(nums.contains(i)){
+                matches++;
+            }
+        }
+        return matches;
+    }
+
+    int points = 0;
+
+    /**
+     * Assigns points to number of matched numbers.
+     *
+     * @return int How many points is the card worth.
+     */
+
+    public int points(){
+        switch(matches){
+            case 0:
+                points = 0;
+                sumOfPoints.add(points);
+                return points;
+            case 1:
+                points = 1;
+                sumOfPoints.add(points);
+                return points;
+            case 2:
+                points = 2;
+                sumOfPoints.add(points);
+                return points;
+            case 3:
+                points = 4;
+                sumOfPoints.add(points);
+                return points;
+            case 4:
+                points = 8;
+                sumOfPoints.add(points);
+                return points;
+            case 5:
+                points = 16;
+                sumOfPoints.add(points);
+                return points;
+            case 6:
+                points = 32;
+                sumOfPoints.add(points);
+                return points;
+            case 7:
+                points = 64;
+                sumOfPoints.add(points);
+                return points;
+            case 8:
+                points = 128;
+                sumOfPoints.add(points);
+                return points;
+            case 9:
+                points = 256;
+                sumOfPoints.add(points);
+                return points;
+            case 10:
+                points = 512;
+                sumOfPoints.add(points);
+                return points;
+            default:
+                return 0;
+        }
+    }
+
+    /**
+     * Adds up points of each card together.
+     *
+     * @return int Total points.
+     */
+    public long sum(){
+        long sum = 0;
+        for(int points: sumOfPoints){
+            sum += points;
+        }
+        return sum;
+    }
+
+
+}
---
+++ Day4/src/Main.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Cards card = new Cards();
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("D:\\Nikolka_skola\\Programování\\Java\\AdventOfCode2023\\Inputs\\input4.txt"));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                card.split(line);
+                card.searchWinNum();
+                card.points();
+            }
+            System.out.println(card.sum());
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+
+       
+
+    }
+}
